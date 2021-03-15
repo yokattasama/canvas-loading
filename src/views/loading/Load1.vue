d7bca30314e0a38afeef825bcf5a1aa899f49282
ml <2246888634@qq.com>
2021-03-15 23:44:00 +0800

    初识zrender

---
+++ src/views/loading/Load1.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <h3>load1</h3>
+    <div ref="zrender" id="zrender" class="zrender"></div>
+  </div>
+</template>
+
+<script>
+import * as zrender from 'zrender/dist/zrender.js';
+export default {
+  name: 'load1',
+  data() {
+    return {
+      group: {},
+      zr: {},
+    }
+  },
+  mounted() {
+    this.zRenderInit();
+    console.log(this.zr)
+    this.group.animate()
+    .when(1000, {
+        x: 100,
+    })
+    .when(1000, {
+        rotation : 15,
+        originX: 250,
+        originY: 250,
+    })
+    // .when(1500, {
+    //   scaleX: 0.5,
+    //   scaleY: 0.5,
+    //   originX: 250,
+    //   originY: 250,
+    // })
+    .start();
+  },
+  methods: {
+    /**初始化 zRender */
+    zRenderInit() {
+      let dom = this.$refs.zrender;
+      this.zr = zrender.init(dom);
+      this.group = new zrender.Group();
+      this.group.x = 250;
+      this.group.y = 250;
+      this.drawTrochoid();
+      this.zr.add(this.group);
+    },
+    /** 绘制函数 */
+    drawTrochoid() {
+      let option = {
+        shape: {
+          cx: 250,
+          cy: 250,
+          r: 80,
+          r0: 35,
+          d: 50,
+          n:"out"
+        },
+        style: {
+          fill: "transparent", // 填充颜色，默认#000
+          stroke: "#ff3040", // 描边颜色，默认null
+          lineWidth: 3, // 线宽， 默认1
+          text :'外旋轮曲线',
+          fontSize:"25",
+          textFill:"red",
+        },
+        hoverable: true, // default true
+        draggable: true,
+      };
+      // 外旋轮线
+      let TrochoidShape = new zrender.Trochoid(option);
+      // 添加外旋轮线到group里
+      this.group.add(TrochoidShape);
+    },
+  }
+}
+</script>
+
+<style lang='less' scoped>
+#zrender {
+  width: 500px;
+  height: 500px;
+  border: 1px solid red;
+  margin: 0 auto;
+}
+</style>
